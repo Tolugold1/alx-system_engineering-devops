60002fcaeddef5337327b6d9adce1bb97c6d1164
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-09-16 11:49:02 +0100

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # script that create a file in /tmp
 file { '/tmp/school':
-       ensure   => 'file',
-       mode     => '0744',
-       owner    => 'www-data',
-       group    => 'www-data',
-       content  => 'I love Puppet',
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
