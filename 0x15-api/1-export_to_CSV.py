7f133093355d096831e9d8501e1a1647ee4be341
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-10-17 18:48:44 +0100

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,13 +8,13 @@ if __name__ == '__main__':
     import csv
 
 
-    Name = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+    N = requests.get("https://jsonplaceholder.typicode.com/users/{}".
                         format(argv[1]))
 
-    Value = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+    V = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
                             format(argv[1]))
-    username = Name.json().get("username")
-    info = Value.json()
+    username = N.json().get("username")
+    info = V.json()
     
     with open("{}.csv".format(argv[1]), "w") as csv_file:
         writer = csv.writer(csv_file, delimiter=',', quotechar='"',
