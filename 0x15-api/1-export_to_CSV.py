1c77bd0d13ac78632b925f3073cc9e1d4cc97878
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-10-17 18:39:26 +0100

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -11,19 +11,14 @@ if __name__ == '__main__':
     urlName = requests.get("https://jsonplaceholder.typicode.com/users/{}".
     format(argv[1]))
 
-    urlValue = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".format(argv[1]))
-    name = urlName.json().get("name")
+    urlValue = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                            format(argv[1]))
+    username = urlName.json().get("name")
     info = urlValue.json()
-
-    total_no_of_task = 0
-    number_of_completed_tasks = 0
-    for task in info:
-        total_no_of_task += 1
-        if task.get('completed'):
-            number_of_completed_tasks += 1
     
     with open("{}.csv".format(argv[1]), "w") as csv_file:
         writer = csv.writer(csv_file)
 
         for task in info:
-            writer.writerow([argv[1], name, task.get("completed"), task.get("title")])
+            writer.writerow([argv[1], username, task.get("completed"), 
+                             task.get("title")])
